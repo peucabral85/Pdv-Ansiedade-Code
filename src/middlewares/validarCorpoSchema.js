747053d83013be5bb0f5a3e39944be50bd99ab34
Pericles Cabral <periclesccabral@gmail.com>
2024-08-02 21:48:40 -0300

    fix(schemas): corrige return de mensagem de erro

--- src/middlewares/validarCorpoSchema.js
+++ src/middlewares/validarCorpoSchema.js
@@ -2,9 +2,9 @@ const validarCorpoSchema = joiSchema => async (req, res, next) => {
     try {
         await joiSchema.validateAsync(req.body);
         next();
-      
+
     } catch (error) {
-        res.status(500).json({mensagem: 'Erro interno no servidor'});
+        return res.status(400).json({ mensagem: error.message });
     }
 }
 
