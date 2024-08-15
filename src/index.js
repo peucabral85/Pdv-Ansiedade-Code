409f79fa4c1154eb4f6e02ce76691f5ad40abdb3
Pericles Cabral <periclesccabral@gmail.com>
2024-08-15 18:36:08 -0300

    feat: adiciona processo para criação de doc swagger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const rotas = require('./routes/rotas');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./docs/swagger_output.json');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use(rotas);
 
 app.listen(process.env.PORT_SERVER, () =>
